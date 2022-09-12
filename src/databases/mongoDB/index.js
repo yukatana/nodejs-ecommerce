591018fec601a3ff020e02dfc1bf1e1daec4dd0c
yukatana <rommel.aranguren@gmail.com>
2022-09-12 20:00:50 -0300

    updated readme

--- src/databases/mongoDB/index.js
+++ src/databases/mongoDB/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 connection = async () => {
-    const URIString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@yukatana.la8n0bl.mongodb.net/eCommerce`
+    const URIString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}/${process.env.MONGODB_DATABASE}`
     await mongoose.connect(URIString)
 }
 
