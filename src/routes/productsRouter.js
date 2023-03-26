dfb5bdbf52188971db6369aaa8dc06f285cf6f18
yukatana <rommel.aranguren@gmail.com>
2023-03-26 19:04:56 -0300

    enabled CORS for GET /api/products/:id

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -4,9 +4,11 @@ const { jwtValidation} = require('../middlewares/auth/passport')
 // bodyValidator middleware class makes sure incoming requests have a valid body
 const bodyValidator = require('../middlewares/bodyValidator')
 const productsController = require('../controllers/productsController')
+// allowing CORS on GET products
+const cors = require('cors')
 
 // GET product by ID, or all products if no param is specified
-productsRouter.get('/:id?', productsController.getProductById)
+productsRouter.get('/:id?', cors(), productsController.getProductById)
 // GET all products pertaining to a specific category
 productsRouter.get('/category/:category', productsController.getByCategory)
 // POST a new product
