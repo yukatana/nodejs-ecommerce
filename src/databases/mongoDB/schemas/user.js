02d2d295416fb1cb022d37a1c54dbd3f536cbd4d
yukatana <rommel.aranguren@gmail.com>
2023-03-29 17:23:48 -0300

    changed user schema

--- src/databases/mongoDB/schemas/user.js
+++ src/databases/mongoDB/schemas/user.js
@@ -1,12 +1,12 @@
 const { Schema, model } = require('mongoose')
 
 const UserSchema = new Schema({
-    firstName : {type: String, required: true},
-    lastName : {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     username: {type: String, required: true},
     password: {type: String, required: true},
-    address: {type: String, required: true},
-    phone: {type: String, required: true},
+    address: {type: String, required: false, default: null},
+    phone: {type: String, required: false, default: null},
     carts: {type: Array, default: []}
 }, {timestamps: true})
 
