3989ff1a1cfc957492ee1a6e410bf130bdee49c7
yukatana <rommel.aranguren@gmail.com>
2023-04-02 21:42:43 -0300

    added cors to serve auth error endpoints

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -10,13 +10,13 @@ authRouter.get('/login', AuthController.serveLogin)
 // POST a login attempt
 authRouter.post('/login', cors(), passportLogin, AuthController.successfulLogin)
 // GET login error page through USE so that redirected requests serve 400 regardless of method
-authRouter.use('/loginError', AuthController.serveLoginError)
+authRouter.use('/loginError', cors(),AuthController.serveLoginError)
 // GET signup form
 authRouter.get('/signup', AuthController.serveSignup)
 // POST a signup attempt
 authRouter.post('/signup', cors(), passportSignup, AuthController.successfulSignup)
 // GET signup error page through USE so that redirected requests serve 400 regardless of method
-authRouter.use('/signupError', AuthController.serveSignupError)
+authRouter.use('/signupError', cors(), AuthController.serveSignupError)
 // GET unauthorized request page through USE so that redirected requests serve 400 regardless of method
 authRouter.use('/unauthorized', AuthController.serveUnauthorizedMessage)
 
