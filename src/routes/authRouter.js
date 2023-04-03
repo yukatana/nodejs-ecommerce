8b884c11561de0d3fa8f6010d2ef2049557b5086
yukatana <rommel.aranguren@gmail.com>
2023-04-02 21:29:10 -0300

    authorized cors on auth endpoints

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,17 +2,18 @@ const { Router } = require('express')
 const authRouter = Router()
 const { passportSignup, passportLogin } = require('../middlewares/auth/passport')
 const AuthController = require('../controllers/authController')
+const cors = require('cors')
 
 // GET login form
 authRouter.get('/login', AuthController.serveLogin)
 // POST a login attempt
-authRouter.post('/login', passportLogin, AuthController.successfulLogin)
+authRouter.post('/login', cors(), passportLogin, AuthController.successfulLogin)
 // GET login error page through USE so that redirected requests serve 400 regardless of method
 authRouter.use('/loginError', AuthController.serveLoginError)
 // GET signup form
 authRouter.get('/signup', AuthController.serveSignup)
 // POST a signup attempt
-authRouter.post('/signup', passportSignup, AuthController.successfulSignup)
+authRouter.post('/signup', cors(), passportSignup, AuthController.successfulSignup)
 // GET signup error page through USE so that redirected requests serve 400 regardless of method
 authRouter.use('/signupError', AuthController.serveSignupError)
 // GET unauthorized request page through USE so that redirected requests serve 400 regardless of method
